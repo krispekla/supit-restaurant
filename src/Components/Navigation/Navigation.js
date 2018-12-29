3f0cd686ed378e7864fad3e4182b3733a2a0d616
krispekla <peklarick@gmail.com>
2018-12-29 12:33:12 +0100

    Default postava strukture

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const navigation = () => {
+  return (
+    <React.Fragment>
+      <p>Bok</p>
+    </React.Fragment>
+  );
+};
+
+export default navigation;
---
+++ src/Containers/Restaurant.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Navigation from "../Components/Navigation/Navigation";
+
+class Restaurant extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navigation />
+        {/* <IntroVideo />
+        <Delivery />
+        <MobileApp />
+        <OurCities />
+        <Subscriptions />
+        <Contact />
+        <Footer /> */}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Restaurant;
