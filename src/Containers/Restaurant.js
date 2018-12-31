0d4339e4bc9f8e0ba8277e2fbd4e4d5fd849ef06
Unknown <peklarick@gmail.com>
2018-12-31 12:31:40 +0100

    adding delivery section

--- src/Containers/Restaurant.js
+++ src/Containers/Restaurant.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Navigation from "../Components/Navigation/Navigation";
 import Intro from "../Components/Intro/Intro";
-
+import Delivery from "../Components/Delivery/Delivery";
 class Restaurant extends React.Component {
   render() {
     return (
       <React.Fragment>
         <Navigation />
         <Intro />
-        {/*  <Delivery />
-        <MobileApp />
+        <Delivery />
+        {/*<MobileApp />
         <OurCities />
         <Subscriptions />
         <Contact />
